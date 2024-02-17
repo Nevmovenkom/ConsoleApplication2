4f90fc8cdff2d5d233f28258888ed23d1d2d6c32
Matvey Nevmovenko <nevmovenkom@bk.ru>
2024-02-18 00:57:17 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+class Vector
+{
+public:
+	Vector()
+	{}
+	Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+	{
+
+	}
+	void show()
+	{
+		std::cout << '\n' << x << '\n' << y << '\n' << z;
+
+		std::cout << '\n' << "model vector:" << ' ' << x * y;
+	}
+private:
+	double x = 0;
+	double y = 0;
+	double z = 0;
+};
+int main()
+{
+	Vector v(10, 10, 10);
+	v.show();
+}
